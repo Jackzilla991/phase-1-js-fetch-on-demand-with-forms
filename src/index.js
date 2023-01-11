8359d318f190e15609a80010a89359d800d06e29
Jack Scarpelli <Festivetomcat99@gmail.com>
2023-01-11 18:58:27 -0500

    commit tree c39f62840d48bc3af6702df59316391409cc53f9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
+    document.querySelector("form").addEventListener("submit", async function(event) {
+      event.preventDefault();
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+      const id = document.querySelector("#searchByID").value;
+      const response = await fetch(`/movie-info?id=${id}`);
+      const data = await response.json();
+  
+      if (response.ok) {
+        document.querySelector("#movieDetails h4").textContent = data.title;
+        document.querySelector("#movieDetails p").textContent = data.summary;
+      } else {
+        alert("Invalid ID or movie not found.");
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
